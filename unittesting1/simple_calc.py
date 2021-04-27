59917ffa56831e44f0ddf40822ebe102dc33c464
Infuro <37084566+Infuro@users.noreply.github.com>
2021-04-27 13:48:38 +0100

    unit testing practice

---
+++ unittesting1/simple_calc.py
@@ -0,0 +1,9 @@
+class SimpleCalc:
+    def add(self, int1, int2):
+        return int1 + int2
+
+    def sub(self, int1, int2):
+        return int1 - int2
+
+    def mult(self, int1, int2):
+        return int1 * int2
